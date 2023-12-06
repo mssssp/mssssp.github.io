9f2f01a000a0e6e5597d5e884ce0e816d76e1032
mssssp <152685589+mssssp@users.noreply.github.com>
2023-12-06 16:08:15 -0700

    fixed menu on phone
    created new specific js to handle only page with offerings

---
+++ services/offerScript.js
@@ -0,0 +1,79 @@
+// Sticky Navbar
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Expandable Service Details
+document.addEventListener('DOMContentLoaded', function() {
+    var expandButtons = document.querySelectorAll('.expand-button');
+    expandButtons.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            var details = this.nextElementSibling;
+            if (details) {
+                details.style.display = details.style.display === 'none' ? 'block' : 'none';
+                this.textContent = this.textContent === 'More Details' ? 'Less Details' : 'More Details';
+            }
+        });
+    });
+
+
+    // Hamburger Menu
+    var hamburger = document.querySelector('.hamburger-menu');
+    var navLinks = document.querySelector('.nav-links');
+    var navLinksItems = document.querySelectorAll('.nav-links li');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+
+    navLinksItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+            }
+        });
+    });
+
+    // Portal Modal
+    var portalLink = document.getElementById('customerPortal');
+    var portalModal = document.getElementById('portalModal');
+    var portalClose = portalModal.querySelector('.close');
+
+    portalLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        portalModal.style.display = "block";
+    });
+
+    portalClose.addEventListener('click', function() {
+        portalModal.style.display = "none";
+    });
+
+    // Add Event Listener to all elements with class 'close'
+    var closeButtons = document.getElementsByClassName("close");
+    Array.from(closeButtons).forEach(function(button) {
+        button.addEventListener('click', function() {
+            var modal = this.closest(".modal");
+            if (modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+
+	var contactBtn = document.querySelector('.contact-btn'); // or '#contact-btn' for an ID
+	if (contactBtn) {
+		contactBtn.addEventListener('click', function() {
+			// Your code here
+		});
+	}
+    
+});
+
